344b0a968e59342ac59de39bacbb7be2c9a94905
ducdosomething <duclehoang211201@gmail.com>
2024-03-25 16:34:20 +0700

    Find max

---
+++ MaxFromFile/Main.java
@@ -0,0 +1,12 @@
+package MaxFromFile;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = ReadAndWriteFile.findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+}
